a2db2eee99e1085af4e8c0808de6644f33946b1b
Michele Satta <michele.satta@edgecasesoftware.co.uk>
2022-01-24 13:41:17 +0000

    Name fix

--- src/main/scala/group/mercator/Checkout.scala
+++ src/main/scala/group/mercator/Checkout.scala
@@ -1,8 +1,8 @@
 package group.mercator
 
 object Checkout {
-  def calculateTotalCost(testSet: List[String]): Double = {
-    testSet.map {
+  def calculateTotalCost(items: List[String]): Double = {
+    items.map {
       case "Apple" => 0.6
       case "Orange" => 0.25
       case _ => 0
