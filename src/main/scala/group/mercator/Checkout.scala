4e6d6c02a6a764a2d89b04138428ec3f45204323
Michele Satta <michele.satta@edgecasesoftware.co.uk>
2022-01-24 14:12:43 +0000

    Second step implemented

--- src/main/scala/group/mercator/Checkout.scala
+++ src/main/scala/group/mercator/Checkout.scala
@@ -1,10 +1,13 @@
 package group.mercator
 
 object Checkout {
+  def appleOffer(number:Int): Int = (number / 2) + (number % 2)
+  def orangeOffer(number:Int): Int = number - number/3
+
   def calculateTotalCost(items: List[String]): Double = {
-    items.map {
-      case "Apple" => 0.6
-      case "Orange" => 0.25
+    items.groupBy(identity).map {
+      case ("Orange", elements) => orangeOffer(elements.size) * 0.25
+      case ("Apple", elements) => appleOffer(elements.size) * 0.6
       case _ => 0
     }.sum
   }
--- src/test/scala/group/mercator/CheckoutTest.scala
+++ src/test/scala/group/mercator/CheckoutTest.scala
@@ -5,16 +5,16 @@ import org.scalatest.wordspec.AnyWordSpec
 class CheckoutTest extends AnyWordSpec {
 
   "The Checkout system" should {
-    "return the right price for a list of Oranges and Apples" in {
-      val testSet = List("Apple", "Apple", "Orange", "Apple")
-      val expectedResult = 2.05
+    "return the right price for a list of Oranges and Apples without offers applied" in {
+      val testSet = List("Apple", "Orange", "Orange")
+      val expectedResult = 1.1
       val actualValue = Checkout.calculateTotalCost(testSet)
       assert(actualValue == expectedResult)
     }
 
     "ignore other type of fruit and return the right price only for Oranges and Apple" in {
-      val testSet = List("Apple", "Apple", "Orange", "Apple", "Lychee")
-      val expectedResult = 2.05
+      val testSet = List("Apple", "Orange", "Orange", "Lychee")
+      val expectedResult = 1.1
       val actualValue = Checkout.calculateTotalCost(testSet)
       assert(actualValue == expectedResult)
     }
@@ -22,6 +22,27 @@ class CheckoutTest extends AnyWordSpec {
     "return 0 for an empty list" in {
       val testSet = List()
       val expectedResult = 0
+      val actualValue = Checkout.calculateTotalCost(testSet)
+      assert(actualValue == expectedResult)
+    }
+
+    "return the right price when only the orange offer is applied" in {
+      val testSet = List("Apple", "Orange", "Orange", "Orange", "Orange", "Orange")
+      val expectedResult = 1.6
+      val actualValue = Checkout.calculateTotalCost(testSet)
+      assert(actualValue == expectedResult)
+    }
+
+    "return the right price when only the apple offer is applied" in {
+      val testSet = List("Apple", "Orange", "Apple", "Apple"," Apple")
+      val expectedResult = 1.45
+      val actualValue = Checkout.calculateTotalCost(testSet)
+      assert(actualValue == expectedResult)
+    }
+
+    "return the right price when both, apple and orange, offers are applied" in {
+      val testSet = List("Apple", "Orange", "Orange", "Orange", "Apple", "Apple"," Apple")
+      val expectedResult = 1.7
       val actualValue = Checkout.calculateTotalCost(testSet)
       assert(actualValue == expectedResult)
     }
